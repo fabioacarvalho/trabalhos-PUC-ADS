87ec991db259bd0a7010994e46594cec1320fa40
Fábio Carvalho <fabioxcarvalho@outlook.com>
2021-08-29 16:02:02 -0300

    update

--- banco-de-dados/sistema-Delivery/populandoESelecionandoDados.sql
+++ banco-de-dados/sistema-Delivery/populandoESelecionandoDados.sql
@@ -15,26 +15,22 @@ insert into Delivery.Entregador(nome, telefone) values
 insert into Delivery.Pedido(quantidade, valorTotal, dia, status, codEntregador) values
 	(2, 7.98, 21/12/05, 1, 2),
     (1, 4.99, 21/12/10, 2, 1),
-    (1, 4.99, 21/08/11, 0, 1),
-    (1, 4.99, 21/08/12, 0, 1);
-    
-insert into Delivery.Pedido(quantidade, valorTotal, dia, status, codEntregador) values
-    (1, 14.99, 21/08/12, 0, 1);
+    (1, 4.99, 21/08/11, 0, 1);
     
 insert into Delivery.CardapioPedido values
 	(1, 1),
     (1, 2),
-    (1, 3),
-    (1, 4);
+    (1, 3);
 
 insert into Delivery.PedidoCliente values
 	(1, 1),
     (1, 2),
-    (2, 3),
-    (1, 4);
+    (2, 3);
 
 #Selecionando dados com filtro por status:
 select * from Delivery.Pedido where status = 0;
 
 #Selecionar apenas os que estao com o status = 0 e juntando com cliente:
-select * from (Delivery.Pedido JOIN Delivery.Cliente) where status = 0;
+select nome, endereco, telefone, codPedido, quantidade, valorTotal, dia, status, codEntregador
+from Delivery.Cliente JOIN Delivery.Pedido on Delivery.Pedido.codPedido = Delivery.Cliente.codCliente 
+where status = 0;
